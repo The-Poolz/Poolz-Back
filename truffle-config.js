b624ac42cbd834fa84329788d698585cde36de52
unknown <salty87@gmail.com>
2020-11-10 11:50:28 +0000

    upload

--- truffle-config.js
+++ truffle-config.js
@@ -24,6 +24,13 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     },
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0xeBCE75948DF6Fe95c5B964c3cDeb71808b615670", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
+    },
     tomotestnet: {
       provider: () =>
         new HDWalletProvider(
