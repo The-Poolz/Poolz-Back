70a613615f3d6d8c13f79b6d7fba8c4a83d6623a
unknown <salty87@gmail.com>
2020-11-10 12:29:18 +0000

    reset

--- truffle-config.js
+++ truffle-config.js
@@ -1,18 +1,60 @@
+/*
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
+ * function when declaring them. Failure to do so will cause commands to hang. ex:
+ * ```
+ * mainnet: {
+ *     provider: function() { 
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
+ *     },
+ *     network_id: '1',
+ *     gas: 4500000,
+ *     gasPrice: 10000000000,
+ *   },
+ */
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+//const teamsMnemonic = "enhance scan dose rib lab jelly damage box museum leaf tail retreat";
+const mnemonic = "peace destroy flock enact stay exotic shop random strike hobby mistake unfold";
+
 module.exports = {
-    // See <http://truffleframework.com/docs/advanced/configuration>
-    // to customize your Truffle configuration!
-    networks: {
-          development: {
-              host: "127.0.0.1",
-              port: 8545,
-              network_id: "*" // Match any network id
-          }
-      },
-      compilers: {
-      solc: {
-        version: "^0.4.24",
-        docker: false,
-        parser: "solcjs",
-      }
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0xeBCE75948DF6Fe95c5B964c3cDeb71808b615670", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
+    },
+    tomotestnet: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          "https://testnet.tomochain.com",
+          0,
+          1,
+          true,
+          "m/44'/60'/0'/0/"
+        ),
+      network_id: "89",
+      gas: 8000000,
+      gasPrice: 10000000000000
     }
-  };
+  },
+  plugins: ["solidity-coverage"],
+  compilers: {
+    solc: {
+      settings: {
+        evmVersion: "byzantium"
+      },
+      version: "^0.4.24",
+      docker: false,
+      parser: "solcjs",
+    }
+  }
+};
