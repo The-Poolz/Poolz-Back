219925c62bcd4fcdba5d781bed7222e72e494a6d
ashwinarora <ashwinarora48@gmail.com>
2021-02-18 20:22:48 +0530

    fixed 0 passing issue

--- test/General/3_getPoolz.js
+++ test/General/3_getPoolz.js
@@ -9,42 +9,43 @@ const rate = new BN('1000000000'); // with decimal21 (shifter) 1 eth^18 = 1 toke
 const amount = new BN('3000000'); //3 tokens for sale
 const invest = web3.utils.toWei('1', 'ether'); //1eth;
 
-contract("Get the Poolz Data", async accounts => {
+contract("Get the Poolz Data", accounts => {
     let instance, fromAddress = accounts[0]
-    let Token = await TestToken.new()
+    let Token 
     
     before(async () => {
         instance = await ThePoolz.new()
+        Token = await TestToken.new()
         let date = new Date();
         date.setDate(date.getDate() + 1);
         await Token.approve(instance.address, amount, { from: accounts[0] });
         await instance.CreatePool(Token.address, Math.floor(date.getTime() / 1000) + 60, rate, rate, amount, 0, zero_address,true,0,0, { from: fromAddress });
-        const value = await instance.poolsCount()
-        // console.log('here', value.toNumber())
-        // assert.equal(value.toNumber(), 1)
     })
 
 
     it('Get Pools IDs of msg.sender', async () => {
         const result = await instance.GetMyPoolsId({from: fromAddress})
-        console.log(result)
-        console.log((await instance.poolsCount()).toNumber())
+        const value = await instance.poolsCount()
+        assert.equal(value.toNumber(), 1)
+        assert.equal(result.length, 1)
     })
 
     it('Get Pools Base Data', async () => {
         const result = await instance.GetPoolBaseData(0)
         const tokenAddress = result[0]
-        assert(tokenAddress, Token.address)
+        assert.equal(tokenAddress, Token.address)
     })
 
     it('Get Pools More Data', async () => {
         const result = await instance.GetPoolMoreData(0)
-        
+        const leftToken = result[1]
+        assert.equal(leftToken.toNumber(), amount.toNumber())
     })
 
     it('Get Pools Extra Data', async () => {
         const result = await instance.GetPoolExtraData(0)
-        
+        const tookLeftOver = result[0]
+        assert.isFalse(tookLeftOver)
     })
 
 })
