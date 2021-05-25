a65561177de42243a7d68c9b33599bdad2002c6a
Ashwin Arora <ashwinarora48@gmail.com>
2021-05-25 20:17:48 +0530

    travis fix

--- bin/test.sh
+++ bin/test.sh
@@ -6,7 +6,7 @@ set -e
 # sleep 5 # to make sure ganache-cli is up and running before compiling
 rm -rf build
 truffle compile
-truffle migrate --reset --network development
+# truffle migrate --reset --network development
 # truffle test
 truffle run coverage
 kill -9 $(lsof -t -i:8545)
